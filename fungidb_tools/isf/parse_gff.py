c264c552af0622647fd200d54b71101c0977620f
edliaw <ed.liaw@gmail.com>
2013-07-16 18:07:02 -0400

    Refactor: renamer class is more logically separated

---
+++ fungidb_tools/isf/parse_gff.py
@@ -0,0 +1,103 @@
+from __future__ import print_function
+import sys
+from warnings import warn
+from collections import OrderedDict
+
+
+class Feature(object):
+    def __init__(self, cols, attr):
+        self.seqid, self.source, self.soterm, self.start, self.end, self.score, self.strand, self.phase = cols
+        self.attr = attr
+
+    def to_gff(self):
+        cols = [self.seqid, self.source, self.soterm, self.start, self.end, self.score, self.strand, self.phase]
+        return cols, self.attr
+
+
+class GFFParser(object):
+    def __init__(self, filetype, fasta=False, comments=True):
+        self.delim = self._set_filetype(filetype)
+        self.fasta = fasta
+        self.comments = comments
+
+    @classmethod
+    def from_args(cls, args):
+        return cls(args.filetype, args.fasta, args.comments)
+
+    @staticmethod
+    def _set_filetype(filetype):
+        delim = {}
+        delim['col'] = '\t'
+        delim['attr'] = ';'
+        if filetype in ('gtf', 'gff2'):
+            delim['key'] = ' '
+            delim['val'] = '"'
+        elif filetype == 'gff3':
+            delim['key'] = '='
+            delim['val'] = ''
+        else:
+            raise Exception('Invalid filetype: must be gtf, gff2, or gff3.')
+        return delim
+
+    def parse(self, infile, commentfile=sys.stdout):
+        col_d = self.delim['col']
+        attr_d = self.delim['attr']
+        key_d = self.delim['key']
+        val_d = self.delim['val']
+
+        for line in infile:
+            # Comments
+            if line.startswith('#'):
+                if line.startswith('##'):
+                    if self.fasta and line.startswith('##FASTA'):
+                        break
+                    print(line, file=commentfile)
+                elif self.comments:
+                    print(line, file=commentfile)
+                continue
+
+            cols = line.rstrip().split(col_d, 9)
+            # Attributes column
+            attr_col = cols.pop()
+            attr = OrderedDict()
+            for pair in attr_col.rstrip(attr_d).split(attr_d):
+                pair = pair.strip()
+                if pair.startswith('#'):
+                    # Try to cut out comments; warn in case it's a bug
+                    warn("WARNING: inline comment present:\n{}".format(line))
+                    break
+                key, val = pair.split(key_d, 2)
+                attr[key] = val.strip(val_d)
+            yield cols, attr
+
+    def parse_features(self, infile, commentfile=sys.stdout):
+        for cols, attr in self.parse(infile, commentfile):
+            yield Feature(cols, attr)
+
+    def join(self, cols, attr):
+        attrs = (self.delim['key'].join((key, surround(val, self.delim['val']))) for key, val in attr.items())
+        cols = cols + [self.delim['attr'].join(attrs),]
+        return self.delim['col'].join(cols)
+
+    def join_feature(self, feature):
+        return self.join(*feature.to_gff())
+
+    def join_all(self, rows):
+        for cols, attr in rows:
+            yield self.join(self, cols, attr)
+
+
+def surround(value, delim):
+    return "{1}{0}{1}".format(value, delim)
+
+
+def add_gff_args(parser):
+    parser.add_argument('-t', '--filetype',
+                        choices=('gtf', 'gff2', 'gff3'), default='gtf',
+                        help='filetype to parse')
+    parser.add_argument('-c', '--comments',
+                        action='store_true',
+                        help='retain comments')
+    parser.add_argument('-f', '--fasta',
+                        action='store_true',
+                        help='retain FASTA sequences')
---
+++ fungidb_tools/isf/rename_contig.py
@@ -0,0 +1,68 @@
+"""Reformat contig name.
+Example
+Input:
+    Supercontig_12.1 of Neurospora crassa OR74A
+Output:
+    NcraOR74A_SC01
+
+2013/07/16
+Edward Liaw
+"""
+import re
+
+class NoMatchException(Exception):
+    pass
+
+
+class ContigRenamer(object):
+    def __init__(self, abbrev, soterm, padding, is_roman=False, regex=None):
+        self.abbrev = abbrev
+        self.soterm = soterm
+        self.padding = "{{:0{:d}d}}".format(padding)
+
+        if is_roman:
+            from .roman import roman_to_int
+            def format_letter(self, letter):
+                return self._format_number(roman_to_int(letter))
+            self._format_letter = format_letter
+
+        if regex is None:
+            regex = r'_(?:(?P<number>\d+)|(?P<letter>[A-Z]+))'
+        self.regex = re.compile(regex)
+
+    @classmethod
+    def from_args(cls, args):
+        return cls(args.species, args.type, args.padding, args.roman, args.regex)
+
+    def _format_number(self, number):
+        return self.padding.format(int(number))
+
+    def _format_letter(self, letter):
+        return letter.upper()
+
+    def rename(self, target):
+        match = self.regex.search(target).groupdict()
+        if 'number' in match:
+            contig = self._format_number(match['number'])
+        elif 'letter' in match:
+            contig = self._format_letter(match['letter'])
+        else:
+            raise NoMatchException("Regular expression didn't find a number or letter.")
+
+        return "{}_{}{}".format(self.abbrev, self.soterm, contig)
+
+
+def add_rename_args(parser):
+    parser.add_argument('--species',
+                        help='species abbreviation')
+    parser.add_argument('--type',
+                        choices=('Chr', 'SC'),
+                        help='SO term')
+    parser.add_argument('--padding',
+                        type=int, default=2,
+                        help='number padding adds 0s to fix the width')
+    parser.add_argument('--roman',
+                        action='store_true',
+                        help='if chromosomes are enumerated by roman numerals')
+    parser.add_argument('--regex',
+                        help='regular expression to search the contig identifier')
