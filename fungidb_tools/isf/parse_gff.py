6f88c46d6f61b226ae6e129e081bd6d3242e610a
edliaw <ed.liaw@gmail.com>
2013-08-01 13:17:20 -0400

    Add parenting to features

--- fungidb_tools/isf/parse_gff.py
+++ fungidb_tools/isf/parse_gff.py
@@ -13,16 +13,53 @@ from __future__ import print_function
 import sys
 from warnings import warn
 from collections import OrderedDict
+from functools import total_ordering
+
+
+class NotChildException(Exception):
+    pass
 
 
 class Feature(object):
-    def __init__(self, cols, attr):
-        self.seqid, self.source, self.soterm, self.start, self.end, self.score, self.strand, self.phase = cols
+    def __init__(self, cols, attr, comment="", children=None):
+        self.seqid, self.source, self.soterm, start, end, self.score, self.strand, self.phase = cols
+        self.pos = [(start, end)]
         self.attr = attr
+        self.comment = comment
+        self.children = [] if children is None else children
 
     def to_gff(self):
-        cols = [self.seqid, self.source, self.soterm, self.start, self.end, self.score, self.strand, self.phase]
-        return cols, self.attr
+        for start, end in self.pos:
+            cols = [self.seqid, self.source, self.soterm, start, end, self.score, self.strand, self.phase]
+            yield cols, self.attr, self.comment
+        for child in self.children:
+            for sub in child.to_gff():
+                yield sub
+
+    def append(self, other):
+        added = False
+        if self.attr['ID'] == other.attr['ID'] and self.soterm == other.soterm:
+            self.pos += other.pos
+            warn("Multi-line feature: %s" % self)
+            added = True
+        else:
+            for parent in other.attr['Parent'].split(','):
+                if self.attr['ID'] == parent:
+                    self.children.append(other)
+                    added = True
+                else:
+                    for child in self.children:
+                        added = child.append(other) or added
+        return added
+
+    def rename(self, name):
+        self.attr['ID'] = name
+        for child in self.children:
+            child.attr['Parent'] = name
+
+    def __str__(self):
+        start, end = self.pos[0]
+        return "{}: {}{}-{}".format(self.seqid, self.strand, start, end)
 
 
 class GFFParser(object):
@@ -58,52 +95,74 @@ class GFFParser(object):
         val_d = self.delim['val']
 
         for line in infile:
-            line = line.rstrip()
-
             # Comments
             if line.startswith('#'):
                 if line.startswith('##'):
-                    if not self.fasta and line.startswith('##FASTA'):
+                    if line.startswith('##FASTA'):
+                        if self.fasta:
+                            commentfile.write(line)
+                            for line in infile:
+                                commentfile.write(line)
                         break
-                    print(line, file=commentfile)
+                    commentfile.write(line)
                 elif self.comments:
-                    print(line, file=commentfile)
+                    commentfile.write(line)
                 continue
 
+            line = line.rstrip()
             # Columns
             cols = line.split(col_d, 9)
             # Attributes column
             attr_col = cols.pop()
             attr = OrderedDict()
+
+            comment = ""
+            c = attr_col.find('#')
+            if c >= 0:
+                if self.comments:
+                    comment = attr_col[c:]
+                else:
+                    warn("WARNING: inline comment present:\n%s" % line)
+                attr_col = attr_col[:c]
+
             for pair in attr_col.rstrip(attr_d).split(attr_d):
                 pair = pair.strip()
-                if pair.startswith('#'):
-                    # Try to cut out comments; warn in case it's a bug
-                    warn("WARNING: inline comment present:\n{}".format(line))
-                    break
                 try:
                     key, val = pair.split(key_d, 2)
                 except:
                     raise Exception("FAILED to split: " + pair)
                 attr[key] = val.strip(val_d)
-            yield cols, attr
+            yield cols, attr, comment
 
     def parse_features(self, infile, commentfile=sys.stdout):
         """Parse lines as Features instead of (columns, attributes)."""
-        for cols, attr in self.parse(infile, commentfile):
-            yield Feature(cols, attr)
+        for cols, attr, comment in self.parse(infile, commentfile):
+            yield Feature(cols, attr, comment)
 
-    def join(self, cols, attr):
+    def parse_3level(self, infile, commentfile=sys.stdout):
+        features = {}
+        for feat in self.parse_features(infile, commentfile):
+            added = Fals
+            try:
+                added = features[feat.attr['Parent']].append(feat)
+            except KeyError:
+                pass
+            if not added:
+                features[feat.attr['ID']] = feat
+        return features
+
+    def join(self, cols, attr, comment=""):
         attrs = (self.delim['key'].join((key, surround(val, self.delim['val']))) for key, val in attr.items())
         cols = cols + [self.delim['attr'].join(attrs),]
-        return self.delim['col'].join(cols)
+        return self.delim['col'].join(cols) + comment
 
     def join_feature(self, feature):
-        return self.join(*feature.to_gff())
+        for cols, attr, comment in feature.to_gff:
+            yield self.join(cols, attr, comment)
 
     def join_all(self, rows):
-        for cols, attr in rows:
-            yield self.join(self, cols, attr)
+        for cols, attr, comment in rows:
+            yield self.join(self, cols, attr, comment)
 
 
 def surround(value, delim):
