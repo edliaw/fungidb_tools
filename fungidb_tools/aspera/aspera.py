1bd89b48cfbf8dd2a7c5e1d3c3c61265f4ddb936
edliaw <ed.liaw@gmail.com>
2013-08-06 18:35:40 -0400

    PEP8

--- fungidb_tools/aspera/aspera.py
+++ fungidb_tools/aspera/aspera.py
@@ -1,6 +1,5 @@
-#!/usr/bin/env python2.7
-# -*- coding: utf-8 -*-
-"""Download files from NCBI using aspera connect (faster than FTP).
+"""\
+Download files from NCBI using aspera connect (faster than FTP).
 
 2013/01/03
 Edward Liaw
