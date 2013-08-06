e6c257f1cf670702f8e05dce8fc2c23a9e00f22a
edliaw <ed.liaw@gmail.com>
2013-08-06 18:36:18 -0400

    PEP8

--- fungidb_tools/datasets/datasets.py
+++ fungidb_tools/datasets/datasets.py
@@ -1,4 +1,5 @@
-"""Common functions used to generate xml dataset configurations
+"""\
+Common functions used to generate xml dataset configurations
 from the FungiDB Genomes spreadsheet.
 
 2013-07-16
@@ -89,9 +90,10 @@ def file_format(fmt):
 
 
 def extract_reps(organisms, debug=False):
-    """Make dictionaries of all the representative species and families. Also,
-    perform some checks to ensure that the representative species are unique
-    and present.
+    """\
+    Make dictionaries of all the representative species and families.
+    Also, perform some checks to ensure that the representative species
+    are unique and present.
     """
     species_reps = {}
     family_reps = {}
@@ -101,32 +103,39 @@ def extract_reps(organisms, debug=False):
         genus_species = naming.short_species(get_row(o, 'fullname'))
         if get_row(o, 'isrefstrain') == "Yes":
             if debug and genus_species in species_reps:
-                raise InvalidFormatException("{} species has too many reference strains: {}.".format(genus_species, (species_reps[genus_species], abbrev)))
+                raise InvalidFormatException(
+                    "{} species has too many reference strains: {}.".format(
+                        genus_species, (species_reps[genus_species], abbrev)))
             species_reps[genus_species] = abbrev
         elif debug and genus_species not in species_reps:
-            raise InvalidFormatException("{} species missing representative or out of order (representative must come first).".format(genus_species))
+            raise InvalidFormatException((
+                "{} species missing representative or out of order"
+                "(representative must come first).").format(genus_species))
 
         # We arbitrarily use the "subclade" as the "family" grouping.
         # Doesn't really map to the taxonomic family.
         family = get_row(o, 'subclade')
         if get_row(o, 'isfamrep') == "Yes":
             if debug and family in family_reps:
-                raise InvalidFormatException("{} family has too many representatives: {}.".format(family, (family_reps[family][0], abbrev,)))
+                raise InvalidFormatException(
+                    "{} family has too many representatives: {}.".format(
+                        family, (family_reps[family][0], abbrev,)))
             family_reps[family] = (abbrev, get_row(o, 'taxid'))
 
     return species_reps, family_reps
 
 
 def old_abbrevs(organisms):
-    """Create a dictionary the maps old abbreviations to their new one.
+    """\
+    Create a dictionary that maps old abbreviations to their new one.
     Useful for changing organisms' abbrevation with scripts.
     """
     sub_abbrev = {}
 
     for o in organisms:
         abbrev = get_row(o, 'abbrev')
-        genus_species = naming.short_species(get_row(o, 'fullname'))
-        subclade = get_row(o, 'subclade')
+        #genus_species = naming.short_species(get_row(o, 'fullname'))
+        #subclade = get_row(o, 'subclade')
 
         old_abbrevs = get_row(o, 'oldabbrevs')
         if old_abbrevs is not None:
