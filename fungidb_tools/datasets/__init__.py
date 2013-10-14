3b6e89a7abf10c726442bbcaac554c03154ca7dc
edliaw <ed.liaw@gmail.com>
2013-10-14 19:34:53 -0400

    Update to reflect changes in datasets.py

--- fungidb_tools/datasets/__init__.py
+++ fungidb_tools/datasets/__init__.py
@@ -1,4 +1,2 @@
-from .datasets import DEFAULT
-from .datasets import COL
-from .datasets import FungiDBXMLGenerator
+from .datasets import DEFAULT, COL, FungiDBXMLGenerator
 from .datasets import bool_from_sheet, str_from_bool
